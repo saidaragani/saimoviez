c93d4182b09b29da873acb2600ba1e43a046005e
saidaragani <saidaragani@gmail.con>
2022-11-26 09:03:45 +0530

    finalsai

---
+++ youtube.js
@@ -0,0 +1,16 @@
+
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   
+  });
+
